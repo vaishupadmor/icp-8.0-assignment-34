3084f44047be963ace27e9dcaa501798f6ea17f1
vaishupadmor <vaishupadmor@gmail.com>
2024-07-14 22:10:38 +0530

    for-loop.js-added

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+const students = [
+    "priti",
+    "vaishnavi",
+    "rahul",
+    "ravi",
+    "rishi",
+    "diksha"
+
+]
+
+for ( const stud of students )
+    {
+     console.log (`hello  ${stud} `)
+    }
+    
