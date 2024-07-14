c69f319f7d2663cef1efddf7e11d638fd017993d
vaishupadmor <vaishupadmor@gmail.com>
2024-07-14 21:44:11 +0530

    index.js-added

---
+++ index.js
@@ -0,0 +1,13 @@
+   const marks = [ 99 , 50 ,180 ,70 ,60,40,302,403,950,458,2890,47,90 ]
+
+   let largest = marks [0]
+
+   for ( const   num of marks )
+   {
+      if ( num >largest )
+   {
+     largest = num
+   }
+   }
+    console.log (largest )
+   
